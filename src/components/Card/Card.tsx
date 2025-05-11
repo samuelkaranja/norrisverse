904f42833386ad6e701d7a9ed7800018940e6f2b
samuelkaranja <samuelkaranja007@gmail.com>
2025-05-11 21:42:41 +0300

    Updated the jokeSlice.ts file

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,33 +2,33 @@ import React, { useEffect } from "react";
 import "./card.css";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../store/Store";
-import { fetchJokes } from "../../store/Slices/jokeSlice";
+import { fetchRandomJoke } from "../../store/Slices/jokeSlice";
 
 const Card: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const jk = useSelector((state: RootState) => state.joke.jokes);
+  const randomJoke = useSelector((state: RootState) => state.joke.randomJoke);
 
   useEffect(() => {
-    dispatch(fetchJokes());
+    dispatch(fetchRandomJoke());
   }, [dispatch]);
 
   const handleJokes = () => {
-    dispatch(fetchJokes());
+    dispatch(fetchRandomJoke());
   };
 
   return (
     <div className="container">
-      {jk?.map((joke) => (
-        <div className="card" key={joke.id}>
+      {randomJoke && (
+        <div className="card" key={randomJoke.id}>
           <img
-            src={joke.icon_url}
+            src={randomJoke.icon_url}
             alt=""
             style={{ width: "50px", height: "50px" }}
           />
-          <h2>"{joke.value}"</h2>
+          <h2>"{randomJoke.value}"</h2>
           <button onClick={handleJokes}>New Joke</button>
         </div>
-      ))}
+      )}
     </div>
   );
 };
--- src/store/Slices/jokeSlice.ts
+++ src/store/Slices/jokeSlice.ts
@@ -10,37 +10,45 @@ interface Joke {
   icon_url: string;
 }
 
+// interface SearchResponse {
+//   total: number;
+//   result: Joke[];
+// }
+
 interface JokeState {
-  jokes: Joke[];
+  randomJoke: Joke | null;
+  categoryJoke: Joke | null;
+  categories: string[];
+  searchResults: Joke[];
   loading: boolean;
   error: string | null;
 }
 
 const initialState: JokeState = {
-  jokes: [],
+  randomJoke: null,
+  categoryJoke: null,
+  categories: [],
+  searchResults: [],
   loading: false,
   error: null,
 };
 
-// Fetch Jokes
+// Fetch Random Jokes
 
-export const fetchJokes = createAsyncThunk<Joke, void, { rejectValue: string }>(
-  "jokes/fetchJokes",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await fetch("https://api.chucknorris.io/jokes/random");
-      if (!response.ok) {
-        throw new Error("Failed to fetch joke");
-      }
-      const result: Joke = await response.json();
-      console.log(result);
-      return result;
-    } catch (error) {
-      console.log(error);
-      return rejectWithValue("Failed to fetch joke");
-    }
+export const fetchRandomJoke = createAsyncThunk<
+  Joke,
+  void,
+  { rejectValue: string }
+>("jokes/fetchRandomJoke", async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    if (!response.ok) throw new Error("Failed to fetch random joke");
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return rejectWithValue("Failed to fetch random joke");
   }
-);
+});
 
 const jokeSlice = createSlice({
   name: "joke",
@@ -48,16 +56,20 @@ const jokeSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchJokes.pending, (state) => {
-        (state.loading = true), (state.error = null);
+      .addCase(fetchRandomJoke.pending, (state) => {
+        state.loading = true;
+        state.error = null;
       })
-      .addCase(fetchJokes.fulfilled, (state, action: PayloadAction<Joke>) => {
+      .addCase(
+        fetchRandomJoke.fulfilled,
+        (state, action: PayloadAction<Joke>) => {
+          state.loading = false;
+          state.randomJoke = action.payload;
+        }
+      )
+      .addCase(fetchRandomJoke.rejected, (state, action) => {
         state.loading = false;
-        state.jokes = [action.payload];
-      })
-      .addCase(fetchJokes.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "An error occurred";
+        state.error = action.payload || "Failed to fetch random joke";
       });
   },
 });
